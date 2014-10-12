564688954a7781f09d6feea23e6b910f916d86f0
Martin Suehowicz <msuehowicz@clearslide.com>
2014-10-12 08:21:52 -0700

    adding more stats

--- curl-stats.sh
+++ curl-stats.sh
@@ -8,11 +8,20 @@ curl_format=$( cat <<EOF
      time_redirect:  %{time_redirect}\n
      time_starttransfer:  %{time_starttransfer}\n
      ----------\n
-     time_total:  %{time_total}\n
+     time_total:  %{time_total}\n\n
+
      http_code: %{http_code}\n
      num_redirects: %{num_redirects}\n
-     redirect_url: %{redirect_url}\n
+     num_connects: %{num_connects}\n
+     url_effective: %{url_effective}\n
+     content_type: %{content_type}\n
+     size_header %{size_header} B\n
+     size_download: %{size_download} B\n
+     speed_download: %{speed_download} B/s\n
+     ssl_verify_result: %{ssl_verify_result}
+
 \n
 EOF
 )
-echo $curl_format | curl -o /dev/null -w @- -s $1 || echo -e "\nUSAGE: $0 http://example.com"
+echo $curl_format | curl -k --location -o /dev/null -w @- -s $1 || echo -e "\nUSAGE: $0 http://example.com"
+
